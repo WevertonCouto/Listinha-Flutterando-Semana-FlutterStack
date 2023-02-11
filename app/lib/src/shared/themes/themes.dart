602af824dc535d0be88e82dc509f454d9dce2dfc
Weverton Couto <wevertoncristais@gmail.com>
2023-02-11 20:19:39 -0300

    add segmented button theme to themes

--- app/lib/src/shared/themes/themes.dart
+++ app/lib/src/shared/themes/themes.dart
@@ -13,13 +13,27 @@ final lightTheme = ThemeData(
     backgroundColor: _lightColorScheme.primary,
     foregroundColor: Colors.white,
   ),
+  segmentedButtonTheme: _segmentedButtonTheme,
 );
 
 final darkTheme = ThemeData(
-  useMaterial3: true,
-  colorScheme: _darkColorScheme,
-  appBarTheme: AppBarTheme(
-    centerTitle: true,
-    backgroundColor: _darkColorScheme.primaryContainer,
-  ),
-);
+    useMaterial3: true,
+    colorScheme: _darkColorScheme,
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      backgroundColor: _darkColorScheme.primaryContainer,
+    ),
+    segmentedButtonTheme: _segmentedButtonTheme,);
+
+SegmentedButtonThemeData get _segmentedButtonTheme => SegmentedButtonThemeData(
+      style: ButtonStyle(
+        textStyle: MaterialStateProperty.resolveWith(
+          (states) {
+            if (states.contains(MaterialState.selected)) {
+              return const TextStyle(fontSize: 9);
+            }
+            return const TextStyle(fontSize: 11);
+          },
+        ),
+      ),
+    );
