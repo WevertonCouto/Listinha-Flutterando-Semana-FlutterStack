7ab9ec2ab9e5ee9586880e77b703724077f8feae
Weverton Couto <wevertoncristais@gmail.com>
2023-02-12 12:52:00 -0300

    add configuration page layout

--- app/lib/src/configuration/configuration_page.dart
+++ app/lib/src/configuration/configuration_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class ConfigurationPage extends StatefulWidget {
   const ConfigurationPage({super.key});
@@ -15,6 +13,66 @@ class _ConfigurationPageState extends State<ConfigurationPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Configurações'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20, right: 20, left: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Configurações',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Tema',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            RadioListTile(
+              value: ThemeMode.system,
+              groupValue: ThemeMode.light,
+              onChanged: (mode) {},
+              title: const Text('Sistema'),
+            ),
+            RadioListTile(
+              value: ThemeMode.dark,
+              groupValue: ThemeMode.light,
+              onChanged: (mode) {},
+              title: const Text(
+                'Claro',
+              ),
+            ),
+            RadioListTile(
+              value: ThemeMode.system,
+              groupValue: ThemeMode.light,
+              onChanged: (mode) {},
+              title: const Text(
+                'Escuro',
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Controle de dados',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: const Text(
+                'Apagar cache e reinciar o app',
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
